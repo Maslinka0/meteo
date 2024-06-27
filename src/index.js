92b41b033e5b72fa057638b3487748aec6f3457a
Maslinka0 <shpolinska@gmail.com>
2024-06-27 18:41:41 +0200

    Added search functionality using the API

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
+function reafreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "09a304930f8t6a9ffa96d124dfof7bde";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(reafreshWeather);
+}
+
 function handeleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
 
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handeleSearchSubmit);
+
+searchCity("Kyiv");
